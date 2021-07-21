b493167ea8b6696e7189076d22cf0c8fdca56d6b
aruvi198 <aruvi198@gmail.com>
2021-07-21 12:40:42 +0530

    Update save_log.py

--- save_log.py
+++ save_log.py
@@ -1,16 +1,21 @@
 import logging
 print("*****************************************************************************************")
 file = open('log.txt','r')
+print("file : ",file)
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 file_handler = logging.FileHandler('/var/log/katib/metrics.log')
 file_handler.setLevel(logging.DEBUG)
 logger.addHandler(file_handler)
 for i in file.readlines():
+    print("checking logs .................................................................")
     if 'loss =' in i:
+        print(i)
         out = i.split(',')[-2]
         print(out)
         logger.info(out)
+    else:
+        print("not found ------------------------------------------------------------------")
         
         
         
