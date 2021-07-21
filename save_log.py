dd366b4cbc358a6fc2ecd3acd5b64f85496c7459
aruvi198 <aruvi198@gmail.com>
2021-07-21 15:36:59 +0530

    Update save_log.py

--- save_log.py
+++ save_log.py
@@ -1,21 +1,30 @@
 import logging
+import tensorflow as tf
+
+logger = tf.get_logger()
+logging.basicConfig(
+    format="%(asctime)s %(levelname)-8s %(message)s",
+    datefmt="%Y-%m-%dT%H:%M:%SZ",
+    level=logging.INFO)
+
 print("*****************************************************************************************")
 file = open('/workspace/tlt-experiments/experiment_dir_unpruned/status.json','r')
 print("file : ",file)
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler('/var/log/katib/metrics.log')
-file_handler.setLevel(logging.DEBUG)
-logger.addHandler(file_handler)
+# logger = logging.getLogger()
+# logger.setLevel(logging.INFO)
+# file_handler = logging.FileHandler('/var/log/katib/metrics.log')
+# file_handler.setLevel(logging.DEBUG)
+# logger.addHandler(file_handler)
 print("checking logs .................................................................")
 for i in file.readlines():
     if 'loss' in i:
         out = i.split(',')[0]
         name = "loss="
         val = str(out.split(":")[-1].strip())
+        logging.info('loss={}'.format(val))
         final = name+val
         print(final)
-        logger.info(final)
+        #logger.info(final)
     else:
 #         print("not found ------------------------------------------------------------------")
         pass
