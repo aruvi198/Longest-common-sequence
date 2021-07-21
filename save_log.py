f61b692e5439363ada709a7c846a2c14439de86f
aruvi198 <aruvi198@gmail.com>
2021-07-21 11:47:51 +0530

    Update save_log.py

--- save_log.py
+++ save_log.py
@@ -1,6 +1,16 @@
+import logging
+print("*****************************************************************************************")
 file = open('log.txt','r')
+logger = logging.get_logger()
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler('logs.log')
+file_handler.setLevel(logging.DEBUG)
+logger.addHandler(file_handler)
 for i in file.readlines():
     if 'loss =' in i:
-        print(i.split(',')[-2])
+        out = i.split(',')[-2]
+        print(out)
+        logger.info(out)
+        
         
         
