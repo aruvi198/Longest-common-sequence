665b91995e1a81467f85d579f6f8eb0ed8e076c8
aruvi198 <aruvi198@gmail.com>
2021-07-22 09:27:30 +0530

    Update modify1.py

--- modify1.py
+++ modify1.py
@@ -4,14 +4,16 @@ parser.add_argument('--num-epochs',default=None,type=int)
 parser.add_argument('--batch-size-per-gpu',default=None,type=int)
 parser.add_argument('--regularizer',default=None,type=str)
 parser.add_argument('--spec-path',default=None,type=str)
+parser.add_argument('--new-spec-path',default=None,type=str)
 args = parser.parse_args()
 flag = 0
-def modify(num_epochs=args.num_epochs,batch_size_per_gpu=args.batch_size_per_gpu,regularizer=args.regularizer,spec_path=args.spec_path):
+def modify(num_epochs=args.num_epochs,batch_size_per_gpu=args.batch_size_per_gpu,regularizer=args.regularizer,spec_path=args.spec_path,new_spec_path=args.new_spec_path):
     print(num_epochs,batch_size_per_gpu)
     home_path,filename = os.path.split(spec_path)
     f = open(spec_path,'r')
     global flag
-    new_path = home_path+"/new_spec.txt"
+#     new_path = home_path+"/new_spec.txt"
+    new_path = home_path+new_spec_path
     print("***************************new path : ",new_path,spec_path)
     f2 = open(new_path,'w')
     for i in f.readlines():
@@ -27,4 +29,4 @@ def modify(num_epochs=args.num_epochs,batch_size_per_gpu=args.batch_size_per_gpu
             i = ' '*i.index('t') + 'type: ' + str(regularizer) + '\n'
             flag = 1
         f2.write(i)
-modify(args.num_epochs,args.batch_size_per_gpu,args.regularizer,args.spec_path)
+modify(num_epochs=args.num_epochs,batch_size_per_gpu=args.batch_size_per_gpu,regularizer=args.regularizer,spec_path=args.spec_path,new_spec_path=args.new_spec_path)
