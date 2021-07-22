aa9f381fc9157fde96c3260dffef4e3ac8b31cc0
aruvi198 <aruvi198@gmail.com>
2021-07-22 20:20:20 +0530

    Update save_log.py

--- save_log.py
+++ save_log.py
@@ -1,3 +1,5 @@
+# Have to add logic to read Validation loss and mAP
+
 import logging
 import tensorflow as tf
 
@@ -10,28 +12,21 @@ logging.basicConfig(
 print("*****************************************************************************************")
 file = open('/workspace/tlt-experiments/experiment_dir_unpruned/status.json','r')
 print("file : ",file)
-# logger = logging.getLogger()
-# logger.setLevel(logging.INFO)
-# file_handler = logging.FileHandler('/var/log/katib/metrics.log')
-# file_handler.setLevel(logging.DEBUG)
-# logger.addHandler(file_handler)
 print("checking logs .................................................................")
-new = open("/workspace/tlt-experiments/log.txt","w")
+
 for i in file.readlines():
     if 'loss' in i:
         out = i.split(',')[0]
         name = "loss="
         val = str(out.split(":")[-1].strip())
-        logging.info('loss={}'.format(val))
-        out = name+val
-        print(out)
-        final = "metricName: {}, metricValue: {}".format("loss",val)
-        logging.info(final)
-        print(final)
-        new.write(final+"\n")
-        #logger.info(final)
+        log = name+val
+        logging.info(log)
+        print(log)
+        
+        log2 = "metricName: {}, metricValue: {}".format("loss",val)
+        logging.info(log2)
+        print(log2)
     else:
-#         print("not found ------------------------------------------------------------------")
         pass
         
         
