15da462910c5cd8a1c8e0012daee8aac79081bf8
aruvi198 <aruvi198@gmail.com>
2021-07-24 21:33:47 +0530

    Update modify1.py

--- modify1.py
+++ modify1.py
@@ -7,9 +7,23 @@ parser.add_argument('--spec-path',default=None,type=str)
 parser.add_argument('--new-spec-path',default=None,type=str)
 args = parser.parse_args()
 flag = 0
-def modify(num_epochs=args.num_epochs,batch_size_per_gpu=args.batch_size_per_gpu,regularizer=args.regularizer,spec_path=args.spec_path,new_spec_path=args.new_spec_path):
-    print(num_epochs,batch_size_per_gpu)
+# def modify(num_epochs=args.num_epochs,batch_size_per_gpu=args.batch_size_per_gpu,regularizer=args.regularizer,spec_path=args.spec_path,new_spec_path=args.new_spec_path):
+def modify(spec_path=args.spec_path,new_spec_path=args.new_spec_path):
     home_path,filename = os.path.split(spec_path)
+    path = "/workspace/tlt-experiments/output_tlt.txt"
+    hp = json.load(path)
+    parameters = hp['currentOptimalTrial']['parameterAssignments']
+    num_epochs_val,batch_size_per_gpu_val,regularizer = None, None, None
+    for i in range(len(parameters)):
+        name = parameters[i]['name']
+        val = parameters[i]['value']
+        if name == "num_epochs":
+            num_epochs_val = val
+        elif name == "batch_size_per_gpu":
+            batch_size_per_gpu_val = val
+        elif name == "regularizer":
+            regularizer_val = val
+    print(num_epochs,batch_size_per_gpu,regularizer)
     f = open(spec_path,'r')
     global flag
 #     new_path = home_path+"/new_spec.txt"
